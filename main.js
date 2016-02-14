606dafe4ca55b0de861a62f3081736ba51194ec2
Justin Willis <jgw9617@gmail.com>
2016-02-14 02:53:04 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+// Authenticating on a global basis.
+const projectId = process.env.GCLOUD_PROJECT_ID; // E.g. 'grape-spaceship-123'
+const gcloud = require('gcloud')({
+    projectId: projectId
+});
+
+const gce = gcloud.compute();
+
+//test
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get("/getVMs", (req, res) => {
+    gce.getVMs( (err, vms) => {
+        // `vms` is an array of `VM` objects.
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log(vms);
+        }
+    });
+});
+
+app.listen(8100, () => {
+    console.log('Example app listening on port 8100!');
+});
